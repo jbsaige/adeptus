f87baa84de617b5a7c870245f143ca38defbdf50
Thomas Wight <thomaswight2@mail.weber.edu>
2014-12-05 03:12:50 -0700

    Added new scenes:  Fire, Void, Air
    Along with scenes added a dynamic spawning of fire bursts for the fire
    scene.  Changed inputs so battle scenes work.  (they were reset when
    merged)

---
+++ Assets/Battle/Scripts/GameController/FireWorldEffects.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireWorldEffects : MonoBehaviour {
+
+    private PlayerStats thePlayer;
+    private bool _isAlive;
+
+    public float startWait;
+    public float fireSpawnRate;
+    public Vector3 spawnValues;
+    public GameObject FireBurstHazard;
+
+	// Use this for initialization
+	void Start () 
+    {
+        thePlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+        StartCoroutine(SpawnFireEvent());
+	}
+
+    IEnumerator SpawnFireEvent()
+    {
+        yield return new WaitForSeconds(startWait);
+        _isAlive = true;
+
+        while (_isAlive)
+        {
+            SpawnFireBursts();
+
+            yield return new WaitForSeconds(fireSpawnRate);
+            if (!thePlayer)
+            {
+                _isAlive = false;
+            }
+
+            if (!_isAlive)
+            {
+                break;
+            }
+        }
+    }
+
+    private void SpawnFireBursts()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, Random.Range(-spawnValues.z, spawnValues.z));
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(FireBurstHazard, spawnPosition, spawnRotation);
+    }
+
+}
